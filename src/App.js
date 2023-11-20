7cd3cf06ad0fdadc60d4e4eba1a84e562c10683f
Anna <a.solovyova.11@gmail.com>
2023-11-20 14:29:45 -0500

    Added GitHub link

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App container">
-      <SearchEngine />
+      <SearchEngine className="App-Engine" />
+      <small class="footer-text">
+        <a href="https://github.com/SoloviovAnna/weather-react">
+          Open source code <span class="anna-text">by Anna S</span>
+        </a>
+      </small>
     </div>
   );
 }
