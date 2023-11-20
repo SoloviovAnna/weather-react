4bf303699da443590e24dd65747314e076d3b57b
Anna <a.solovyova.11@gmail.com>
2023-11-20 17:46:18 -0500

    Added container class

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App container">
-      <SearchEngine className="App-Engine" />
-      <small class="footer-text">
-        <a href="https://github.com/SoloviovAnna/weather-react">
-          Open source code <span class="anna-text">by Anna S</span>
-        </a>
-      </small>
+    <div className="App">
+      <div className="container">
+        <SearchEngine className="App-Engine" />
+        <small class="footer-text">
+          <a href="https://github.com/SoloviovAnna/weather-react">
+            Open source code <span class="anna-text">by Anna S</span>
+          </a>
+        </small>
+      </div>
     </div>
   );
 }
